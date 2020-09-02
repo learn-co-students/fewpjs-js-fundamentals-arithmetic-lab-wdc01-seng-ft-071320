4b881b0c9f15a717276e7cbd0d40dd5b747b52a6
ClaFiore <claborghini@gmail.com>
2020-09-02 14:52:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+multiply
 
-// Write your code here
+let random = Math.ceil(Math.random())
+random
+
+let mod = 10 % 6
+mod
+
+let max = Math.max(1,3,20, 5, 8)
+max
