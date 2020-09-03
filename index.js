f0fca9d18b851a653b96ce771a3877f3619cabc6
solomonfy <fantahunsolomon@gmail.com>
2020-09-03 16:08:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+
+let num1 = 2
+let num2 = 31
+
+let multiply = (num1 * num2)
+
+
+let random = (Math.random() + 1)
+
+let mod = 14 % 5
+
+let max = Math.max(1, 5, 4, 20)
